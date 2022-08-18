5918ff19a6653abbfbe461f3599ceea86f30164c
Thiago <thiivieira@gmail.com>
2022-08-18 17:57:09 -0300

    feat: criando estrutura em poo da calculadora

---
+++ src/controller/CalcController.js
@@ -0,0 +1,23 @@
+class CalcCalculadora{
+
+    constructor(){
+        this._displayCalc = '0'
+        this._dataAtual;
+    }
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(value){
+        this._displayCalc = value;
+    }
+
+    get dataAtual(){
+        return this._dataAtual;
+    }
+
+    set dataAtual(data){
+        this._dataAtual = data
+    }
+}
