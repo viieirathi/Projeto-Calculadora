52fffda73b49b46a3add3171f3e60b87059c6de9
Thiago <thiivieira@gmail.com>
2022-09-01 14:57:13 -0300

    feat: inserindo valores no painel da calc

--- src/controller/CalcController.js
+++ src/controller/CalcController.js
@@ -1,12 +1,12 @@
 class CalcCalculator {
   constructor() {
+    this._operation = [];
     this._displayCalcEl = document.querySelector("#display");
     this._dateEl = document.querySelector("#data");
     this._timeEl = document.querySelector("#hora");
     this._currentDate;
     this.initialize();
     this.initButtonsEvent();
-    this._operation = [];
   }
 
   initialize() {
@@ -14,6 +14,7 @@ class CalcCalculator {
     setInterval(() => {
       this.setDisplayDateTime();
     }, 1000);
+    this.setLastNumberToDisplay();
   }
 
   addEventListenerAll(element, events, fn) {
@@ -24,14 +25,91 @@ class CalcCalculator {
 
   clearAll() {
     this._operation = [];
+    this.setLastNumberToDisplay();
   }
 
   clearEntry() {
     this._operation.pop();
+    this.setLastNumberToDisplay();
+  }
+
+  getLastOperation() {
+    return this._operation[this._operation.length - 1];
+  }
+
+  setLastOperation(value) {
+    this._operation[this._operation.length - 1] = value;
+  }
+
+  isOperator(value) {
+    return (["+", "-", "*", "%", "/"].indexOf(value) > -1);
+  }
+
+  pushOperation(value) {
+    this._operation.push(value);
+    if (this._operation.length > 3) {
+      this.calc();
+    }
+  }
+
+  calc() {
+    let last = "";
+    if (this._operation.length > 3) {
+      last = this._operation.pop();
+    }
+
+    let result = eval(this._operation.join(""));
+
+    if (last == "%") {
+      result /= 100;
+      this._operation = [result];
+    } else {
+      this._operation = [result];
+      if (last) this._operation.push(last);
+    }
+    this.setLastNumberToDisplay();
+  }
+
+
+  setLastNumberToDisplay() {
+    let lastNumber;
+
+    for (let i = this._operation.length - 1; i >= 0; i--) {
+
+      if (!this.isOperator(this._operation[i])) {
+
+        lastNumber = this._operation[i];
+
+        break;
+
+      }
+
+    }
+
+    if (!lastNumber) lastNumber = 0;
+
+    this.displayCalc = lastNumber;
   }
 
   addOperation(value) {
-    this._operation.push(value);
+    if (isNaN(this.getLastOperation())) {
+      if (this.isOperator(value)) {
+        this.setLastOperation(value);
+      } else if (isNaN(value)) {
+        console.log("aqui elseif", value);
+      } else {
+        this.pushOperation(value);
+        this.setLastNumberToDisplay();
+      }
+    } else {
+      if (this.isOperator(value)) {
+        this.pushOperation(value);
+      } else {
+        let newValue = this.getLastOperation().toString() + value.toString();
+        this.setLastOperation(parseInt(newValue));
+        this.setLastNumberToDisplay();
+      }
+    }
   }
 
   setError() {
@@ -47,25 +125,25 @@ class CalcCalculator {
         this.clearEntry();
         break;
       case "soma":
-
+        this.addOperation("+");
         break;
       case "subtracao":
-
+        this.addOperation("-");
         break;
       case "multiplicacao":
-
+        this.addOperation("*");
         break;
       case "divisao":
-
+        this.addOperation("/");
         break;
-      case "porcetagem":
-
+      case "porcento":
+        this.addOperation("%");
         break;
       case "igual":
-
+        this.calc();
         break;
       case "ponto":
-        
+        this.addOperation(".");
         break;
       case "0":
       case "1":
